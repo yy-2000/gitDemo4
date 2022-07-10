aaf25a44a76d3f97e6fd2f50caeb8b285f65a2c7
TestGit <TestGit@test.com>
2022-07-10 19:58:24 +0800

    first maven test git

---
+++ src/main/java/com/yy/Test.java
@@ -0,0 +1,11 @@
+package com.yy;
+
+/**
+ * @author Marston
+ * @date 2022/7/10
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("maven test git1");
+    }
+}
