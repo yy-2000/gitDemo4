cab0202805cd9c8c2a28fdcb0c793b5667495f44
TestGit <TestGit@test.com>
2022-07-10 20:20:20 +0800

    hotfix commit 2

--- src/main/java/com/yy/Test.java
+++ src/main/java/com/yy/Test.java
@@ -9,5 +9,6 @@ public class Test {
         System.out.println("maven test git1");
         System.out.println("maven test git2");
         System.out.println("maven test git4");
+        System.out.println("hot-fix 冲突合并");
     }
 }
